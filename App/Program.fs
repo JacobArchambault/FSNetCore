b1f9c0f5b1e44924ac72579ccf6306c3a02647ff
Jacob Archambault <jacob.archambault@waystar.com>
2020-02-11 09:19:00 -0500

    Add project files.

---
+++ App/Program.fs
@@ -0,0 +1,12 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open Library
+
+[<EntryPoint>]
+let main argv =
+    printfn "Json converter!"
+    argv 
+    |> Array.map getJsonNetJson
+    |> Array.iter (printfn "%s")
+    0 // return an integer exit code
---
+++ FSNetCore/Library.fs
@@ -0,0 +1,5 @@
+﻿module Library
+open Newtonsoft.Json
+
+let getJsonNetJson value =
+    sprintf "I used to be %s but now I'm %s thanks to JSON!" value (JsonConvert.SerializeObject(value))
